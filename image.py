efc087b92e26b901796eaa179b77342cbff341e1
Karl Cristina Barraza Lopez <barraza.karla1234@gmail.com>
2025-01-06 16:35:25 -0800

    move image functions into a class

--- image.py
+++ image.py
@@ -1,12 +1,24 @@
 from PIL import Image, ImageOps
 
-def load_image(filepath): return Image.open(filepath) 
+class StitchImage: 
+    def __init__ (self, filepath):
+        self.originalimage = Image.open(filepath)
+        self.currentImage = self.originalimage
+        # self.originalX = self.originalimage.get_image_dimensions
 
+    def load_image(filepath): return Image.open(filepath) 
 
-def get_image_dimensions(image): return image.size
+    #returns image dimensions of the originally provided image
+    def get_image_dimensions(self): return self.originalimage.size
 
-def get_pixel_layer(image): return image.load()
+    def get_pixel_layer(self): return self.originalimage.load()
 
-def resize_image(image, newX, newY): return ImageOps.contain(image, (int(newX), int(newY)))
+    #"Returns a resized version of the image, set to the maximum width and height within the requested size, while maintaining the original aspect ratio."
+    # https://pillow.readthedocs.io/en/stable/reference/ImageOps.html#PIL.ImageOps.contain
+    def resize_image(self,newX, newY): 
+        print(f"Resizing image to: {newX}, {newY}")
+        self.currentImage = ImageOps.contain(self.originalimage, (int(newX), int(newY)))
+        return self.currentImage
 
-def save_image(filepath): return Image.save()
+    #saves the current state of the image
+    def save_image(self,filepath): return self.currentImage.save(filepath)
--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import argparse
 import cleanup
 import os
-import image
-
+from image import StitchImage
 # use this to test: python main.py sskaeeoh_tshirt.png 400 500 
 
 def crossStitchParser(): 
@@ -12,7 +11,7 @@ def crossStitchParser():
     parser.add_argument('newfilename', type=str, help='the new filename to save your resized file, saved under build/')
     parser.add_argument('y', type=int, help='the number of desired pixels on the y axis')
     parser.add_argument('x', type=int, help = 'the number of desired pixels on the x axis')
-    parser.add_argument('clean', type=bool, default = True, help='default True, deletes previously generated images under build/')
+    parser.add_argument('--clean', type=bool, default = False, help='optional, default False, deletes previously generated images under build/')
     return parser
 
 
@@ -30,9 +29,12 @@ if __name__ == "__main__":
 
     print(f"Generating a {args.x} x {args.y} crossstitch pattern for {args.filepath} ")
     
-    original_image = image.load_image(args.filepath)
-    resized_image = image.resize_image(original_image, args.x, args.y)
-    pixel_layer = image.get_pixel_layer(resized_image)
+    myImage = StitchImage(args.filepath)
+    myImage.resize_image(args.x, args.y)
+    pixel_layer = myImage.get_pixel_layer()
+
     os.makedirs("build", exist_ok=True)
+    
     outputfile = "build/" + args.newfilename
-    resized_image.save("build/blahblahblah.png")
+    
+    myImage.save_image(outputfile)
